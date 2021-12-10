8b378edc998e39036c361e73f29530f1b44d2421
abelcg <abelcg86@gmail.com>
2021-12-10 16:18:21 -0300

    se creo la rama crearNoticia

--- src/controllers/noticia.controllers.js
+++ src/controllers/noticia.controllers.js
@@ -1,10 +1,11 @@
 const noticiaCtrl = {};
 
-noticiaCtrl.nuevaNoticia = (req, res) => {
-  res.send("alguien quiere agregar una noticia");
-};
 noticiaCtrl.borrarNoticia = (req, res) => {
   res.send("alguien quiere borrar una noticia");
+};
+
+noticiaCtrl.nuevaNoticia = (req, res) => {
+  res.send("alguien quiere agregar una noticia");
 };
 
 export default noticiaCtrl;
