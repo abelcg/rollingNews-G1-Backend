eb5094c130aa363fedbe4c1874ad56e4d5e9d845
Andy <andysatie@gmail.com>
2021-12-22 15:42:49 -0300

    commit provisorio para un modelo de datos para categorias

---
+++ src/models/categoria.js
@@ -0,0 +1,18 @@
+import mongoose, {Mongoose, Schema} from 'mongoose';
+
+const categoriaSchema = new Schema({
+    nombre:{
+        required: true,
+        type: String,
+        maxlength: 50
+    },
+    descripcion:{
+        required: true,
+        type: String,
+        maxlength: 100
+    }
+});
+
+const Categoria = mongoose.model('categoria', categoriaSchema);
+
+export default Categoria;
